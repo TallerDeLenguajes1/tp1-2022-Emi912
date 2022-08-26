4ef2e42beb9476020dad50ed60d287cc817a2a3d
Emilio Merchan <emimerchan83@gmail.com>
2022-08-26 19:12:19 -0300

    Problema 1

---
+++ EjerciciosConsola/Problema1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un numero para calcular su cuadrado");
+int num;
+try
+{
+    num = Convert.ToInt32(Console.ReadLine());
+    int cuadrado = CalcularCuadrado(num);
+    Console.WriteLine("El cuadrado del numero ingresado es: " + cuadrado);
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine("Error: " +  ex.Message);
+}
+
+
+
+int CalcularCuadrado(int num)
+{
+    return num * num;
+}
